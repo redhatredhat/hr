f44057a11ecf5414e1529f4431e9e9ad26c3b3fb
wym <yongmaowang1973@sohu.com>
2018-04-23 23:27:00 +0800

    add Hell2.java

---
+++ src/gitTest/Hello2.java
@@ -0,0 +1,14 @@
+package gitTest;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		System.out.println("Hello!");
+		System.out.println("Hello!");
+		System.out.println("Hello!");
+		System.out.println("Hello!");
+		System.out.println("Hello!");
+	}
+
+}
